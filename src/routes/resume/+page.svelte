c0b8e3ca47b78a6752f31cb5890dc6d0c968d8b1
jwillesen <git@wizardwell.net>
2025-05-17 21:50:14 -0700

    amplify the resume page so it's more awesome

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,6 +2,9 @@
   import ContactInformation from "./contact-information.svelte"
   import Skills from "./skills.svelte"
   import Projects from "./projects.svelte"
+  import { Accordion } from "@skeletonlabs/skeleton-svelte"
+
+  let openAccordions: string[] = $state([])
 </script>
 
 <h2 class="h2 sr-only">Resume</h2>
@@ -14,25 +17,39 @@
       accessibility, and technical elegance.
     </p>
   </section>
-  <hr />
-  <section>
-    <h3 class="h3">Contact Information</h3>
-    <ContactInformation />
-  </section>
-  <hr />
-  <section>
-    <h3 class="h3">Technical Skills</h3>
-    <Skills />
-  </section>
-  <hr />
-  <section>
-    <h3 class="h3">Featured Projects</h3>
-    <Projects />
-  </section>
-</div>
+  <!-- <hr /> -->
 
-<style>
-  h3 {
-    text-decoration: underline;
-  }
-</style>
+  <Accordion
+    value={openAccordions}
+    multiple
+    onValueChange={(e) => (openAccordions = e.value)}
+  >
+    {#snippet iconClosed()}
+      <i class="fa-regular fa-angle-left btn preset-filled-primary-500"></i>
+    {/snippet}
+    {#snippet iconOpen()}
+      <i class="fa-regular fa-angle-down btn preset-filled-primary-500"></i>
+    {/snippet}
+    <Accordion.Item value="contact">
+      {#snippet lead()}<i class="h3 fa-regular fa-address-card"></i>{/snippet}
+      {#snippet control()}<h3 class="h3">Contact Information</h3>{/snippet}
+      {#snippet panel()}<ContactInformation />{/snippet}
+    </Accordion.Item>
+
+    <Accordion.Item value="skills">
+      {#snippet lead()}
+        <i class="h3 fa-regular fa-screwdriver-wrench"></i>
+      {/snippet}
+      {#snippet control()}<h3 class="h3">Technical Skills</h3>{/snippet}
+      {#snippet panel()}<Skills />{/snippet}
+    </Accordion.Item>
+
+    <Accordion.Item value="projects">
+      {#snippet lead()}
+        <i class="h3 fa-regular fa-shelves"></i>
+      {/snippet}
+      {#snippet control()}<h3 class="h3">Featured Projects</h3>{/snippet}
+      {#snippet panel()}<Projects />{/snippet}
+    </Accordion.Item>
+  </Accordion>
+</div>
